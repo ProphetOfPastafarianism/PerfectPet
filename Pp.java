e0784295702109c6af79e62888b223735c4354af
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-10-13 18:29:36 +0000

    fisrst

---
+++ Pp.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Pp
+{
+    public static void main(String[] args)
+    {
+        //ask user for info 
+        //makes the scanner
+    Scanner myObj = new Scanner(System.in);
+    //makes the code look cool and interactive
+    System.out.println("Favorite color:");
+    //gets the color
+    String color = myObj.nextLine();
+    //makes the code look cool and interactive
+    System.out.println("Favorite season:");
+    //gets the season
+    String season = myObj.nextLine();
+    //same as before
+    System.out.println("Your name:");
+    //gets ur name
+    String name = myObj.nextLine();
+    if (color=="blue")
+    {
+        if (season=="fall")    
+    {    
+        System.out.println("you get an alligator!");
+    }
+
+
+}
+
+   
+    
+
+       
+       
+        //make sure its valid
+        
+
+
+    }
+
+
+}
